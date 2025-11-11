751ab59111a2384de6e5e4c9806e47e6d35cacc3
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-11-11 15:52:26 -0500

    añadiendo mas colores principales. agrego lista de almacenes como compoente a parte

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,8 +1,12 @@
+import { ListaAlmacenes } from "./ListaAlmacenes"
+
 export const Sidebar = () => {
   return (
-    <div className='flex flex-col w-[330px] h-screen bg-primary-black'>
-      <div className='flex items-center justify-center h-20'>
+    <div className='flex flex-col w-80 h-screen bg-primary-black py-10 px-3 gap-2'>
+      <div className='flex flex-col items-center justify-center gap-4'>
         <h2 className="text-4xl text-primary-white font-bold">Almacenes</h2>
+        <ListaAlmacenes/>
+        <button className="rounded-md py-2 px-4 cursor-pointer bg-primary-400 text-primary-white hover:bg-primary-500">Agregar almacen</button>
       </div>
     </div>
   )
