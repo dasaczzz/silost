2144f2ced1e8a5ae4508a8052baac2b219c6c977
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-11-11 15:53:57 -0500

    simplificando main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Sidebar } from './componentes/Sidebar.jsx'
+import { Silost } from './Silost'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Sidebar />
-  </StrictMode>,
+    <Silost />
+  </StrictMode>
 )
