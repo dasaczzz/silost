7f161f7bab345ef1df9047d4641420cd0df86196
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-11-12 15:29:10 -0500

    agregando navbar

---
+++ src/componentes/genericos/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex py-4 px-8 bg-primary-400 text-primary-white items-start">
+      <h1 className="font-title text-5xl">Silost</h1>
+    </nav>
+  )
+}
